846a0ce20f806f71d809bd3226c4e7e26e27d714
“fenn7” <afenn666@gmail.com>
2023-07-14 00:31:43 +0100

    feat: add dimension displacement

--- src/main/java/fenn7/grenadesandgadgets/commonside/entity/misc/TemporalFissureEntity.java
+++ src/main/java/fenn7/grenadesandgadgets/commonside/entity/misc/TemporalFissureEntity.java
@@ -7,21 +7,21 @@ import fenn7.grenadesandgadgets.commonside.entity.GrenadesModEntities;
 import fenn7.grenadesandgadgets.commonside.util.GrenadesModUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
-import net.minecraft.entity.mob.SpiderEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import org.apache.logging.log4j.core.jmx.Server;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -34,9 +34,10 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 public class TemporalFissureEntity extends Entity implements IAnimatable {
     public static final TrackedData<Integer> DIMENSION_KEY = DataTracker.registerData(TemporalFissureEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    public static final String DIM_KEY = "dimKey";
     private static final float DISPLACEMENT_DAMAGE = 10.0F;
     private static final float MAX_DISPLACEMENT_THRESHOLD = 100.0F;
-    private static final float MAX_CATASTROPHIC_DAMAGE = 70.0F;
+    private static final float MAX_CATASTROPHIC_DAMAGE = 80.0F;
     private static final int MAX_LIFE_BASE = 800;
     private static final int EXTRA_LIFE_PER_RANGE = 20;
     private static final int SECONDS_BETWEEN_SPREAD = 15;
@@ -65,20 +66,47 @@ public class TemporalFissureEntity extends Entity implements IAnimatable {
     public void tick() {
         super.tick();
         if (this.age > MAX_LIFE_BASE + (this.range * EXTRA_LIFE_PER_RANGE)) {
-            this.remove(RemovalReason.DISCARDED);
+            this.discard();
         }
         var affectedBlocks = this.getOrCreateAffectedBlocks();
         affectedBlocks.forEach(
-            pos -> {}
+            pos -> {
+            }
         );
         // suck in entities with particle effect
         // overwrite blocks
         // decelerate entities
         // expand to 3x3 size
+        if (!this.world.isClient) {
+            if (this.world.equals(this.world.getServer().getWorld(World.END)) && this.squaredDistanceTo(Vec3d.ofCenter(ServerWorld.END_SPAWN_POS)) <= 25) {
+                this.discard();
+            }
+            this.handleEntityCollisions();
+        }
+    }
+
+    private void handleEntityCollisions() {
         Box collisionBox = this.getDimensions(this.getPose()).getBoxAt(this.getPos());
-        this.world.getNonSpectatingEntities(Entity.class, collisionBox).forEach(
-            entity -> entity.damage(GrenadesModDamageSources.DIMENSIONAL_DISPLACEMENT, DISPLACEMENT_DAMAGE)
-        );
+        this.world.getNonSpectatingEntities(Entity.class, collisionBox).stream()
+            .filter(entity -> !(entity instanceof TemporalFissureEntity)).forEach(entity -> {
+                ServerWorld destination = this.getDestinationWorld();
+                boolean worldsMatch = this.world.getDimension().equals(destination.getDimension());
+                if (entity instanceof LivingEntity alive) {
+                    var displacementStats = this.calculateDisplacementDamage(alive, worldsMatch);
+                    alive.damage(GrenadesModDamageSources.DIMENSIONAL_DISPLACEMENT, displacementStats.getLeft());
+                    if (displacementStats.getRight()) {
+                        this.discard();
+                    }
+                }
+                if (!worldsMatch) {
+                    if (destination.equals(this.world.getServer().getWorld(World.NETHER))) {
+                        entity.setInNetherPortal(entity.getBlockPos());
+                        entity.resetNetherPortalCooldown();
+                        // TODO: DESTROY PORTAL ?
+                    }
+                    entity.moveToWorld(destination);
+                }
+            });
     }
 
     @Override
@@ -98,17 +126,23 @@ public class TemporalFissureEntity extends Entity implements IAnimatable {
 
     @Override
     protected void readCustomDataFromNbt(NbtCompound nbt) {
-
+        this.dataTracker.set(DIMENSION_KEY, nbt.getInt(DIM_KEY));
     }
 
     @Override
     protected void writeCustomDataToNbt(NbtCompound nbt) {
-
+        nbt.putInt(DIM_KEY, this.dataTracker.get(DIMENSION_KEY));
     }
 
     @Override
     public Packet<?> createSpawnPacket() {
         return new EntitySpawnS2CPacket(this);
+    }
+
+    private Pair<Float, Boolean> calculateDisplacementDamage(LivingEntity entity, boolean shouldHalve) {
+        boolean cantDisplace = entity.getMaxHealth() > MAX_DISPLACEMENT_THRESHOLD || !entity.canUsePortals();
+        float baseDamage = cantDisplace ? MAX_CATASTROPHIC_DAMAGE : DISPLACEMENT_DAMAGE;
+        return new Pair<>(shouldHalve ? baseDamage / 2F : baseDamage, cantDisplace);
     }
 
     private ServerWorld getDestinationWorld() {
--- src/main/java/fenn7/grenadesandgadgets/commonside/tags/GrenadesModTags.java
+++ src/main/java/fenn7/grenadesandgadgets/commonside/tags/GrenadesModTags.java
@@ -1,6 +1,7 @@
 package fenn7.grenadesandgadgets.commonside.tags;
 
 import fenn7.grenadesandgadgets.commonside.GrenadesMod;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -8,6 +9,17 @@ import net.minecraft.util.registry.Registry;
 
 public class GrenadesModTags {
     public static class Blocks {
+        public static final TagKey<Block> NETHER_FISSURE_CORRUPTION = createCommonTag("dim-1_fissure_corruption");
+        public static final TagKey<Block> OVERWORLD_FISSURE_CORRUPTION = createCommonTag("dim0_fissure_corruption");
+        public static final TagKey<Block> END_FISSURE_CORRUPTION = createCommonTag("dim1_fissure_corruption");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier(GrenadesMod.MOD_ID, name));
+        }
+
+        private static TagKey<Block> createCommonTag(String name) {
+            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
+        }
     }
 
     public static class Items {
